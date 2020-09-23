be488e64a5f9656e3c8f534929ec2662d3ea10ee
Roland Betz <68927907+rolandbetz@users.noreply.github.com>
2020-09-23 20:34:53 +0200

    Add files via upload

---
+++ Headers/Base.h
@@ -0,0 +1,21 @@
+#ifndef BASE_H_INCLUDED
+#define BASE_H_INCLUDED
+#include <vector>
+
+class Base{
+protected:
+    int column, row, max_intensity;
+    std::string image_name;
+    std::vector <std::vector<int> > vect;
+
+public:
+    Base();
+
+    void load();
+
+    virtual void operation()=0;
+
+    void save();
+};
+
+#endif // BASE_H_INCLUDED
---
+++ Headers/Darkening.h
@@ -0,0 +1,10 @@
+#ifndef DARKENING_H_INCLUDED
+#define DARKENING_H_INCLUDED
+#include "Base.h"
+
+class Darkening : public Base{
+public:
+    void operation();
+};
+
+#endif // DARKENING_H_INCLUDED
