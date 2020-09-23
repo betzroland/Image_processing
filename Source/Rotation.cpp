e9cc63b3f51245e4c3893c01bbd07f14fabef8d7
Roland Betz <68927907+rolandbetz@users.noreply.github.com>
2020-09-23 20:34:24 +0200

    Add files via upload

---
+++ Source/Rotation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Rotation.h"
+
+using namespace std;
+
+Rotation::Rotation(){
+    image_copyvector=vect;
+}
+
+void Rotation::operation(){
+
+    int angle_of_rotation;
+    cout << "Angle_of_rotation? (90, 180, 270)" << endl;
+    cin >> angle_of_rotation;
+
+    switch(angle_of_rotation){
+        case 90 :
+        Rotation::rotate_by_90();
+        image_name=image_name+"_rotated_90.pgm";
+        break;
+
+        case 180 :
+        Rotation::rotate_by_180();
+        image_name=image_name+"_rotated_180.pgm";
+        break;
+
+        case 270 :
+        Rotation::rotate_by_270();
+        image_name=image_name+"_rotated_270.pgm";
+    }
+}
+
+void Rotation::rotate_by_90(){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<column; j++){
+            vect[i][j]=image_copyvector[j][(column-1)-i];
+        }
+    }
+}
+
+void Rotation::rotate_by_180(){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<column; j++){
+            vect[i][j]=image_copyvector[(row-1)-i][(column-1)-j];
+        }
+    }
+}
+
+void Rotation::rotate_by_270(){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<column; j++){
+            vect[i][j]=image_copyvector[(row-1)-j][i];
+        }
+    }
+}
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,92 +1,20 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <stdexcept>
-#include <cstdio>
-#include "Read_in.h"
-#include "Lights.h"
-#include "Transform.h"
-#include "Convolve.h"
+#include "Base.h"
+#include "Darkening.h"
+#include "Rotation.h"
 
 using namespace std;
 
-int main(){
+int main()
+{
+    /*Darkening darkened;
+    darkened.load();
+    darkened.operation();
+    darkened.save();*/
 
-string next="y";
-
-while(next=="y"){
-
-    Image image;
-    image.load();
-
-    string functions;
-    cout << "What kind of function do you want to apply? (Light, Transform, Convolution)" << endl;
-    cin >> functions;
-
-    string str;
-
-    if(functions=="Light"){
-        Lights light;
-        cout << "Choose a specific operation! (Darken, Brighten, Invert, Contrast+)" << endl;
-        cin >> str;
-        if(str=="Darken"){
-            light.darken(image);
-            image.save();
-        }
-        else if(str=="Brighten"){
-            light.brighten(image);
-            image.save();
-        }
-        else if(str=="Invert"){
-            light.invert(image);
-            image.save();
-        }
-        else if(str=="Contrast+"){
-            light.contrast(image);
-            image.save();
-        }
-    }
-    else if(functions=="Transform"){
-        Transform transf(image);
-        cout << "Choose a specific operation! (Rotate, Mirror)" << endl;
-        cin >> str;
-        if(str=="Rotate"){
-            transf.rotation(image);
-            image.save();
-        }
-        else if(str=="Mirror"){
-            transf.mirroring(image);
-            image.save();
-        }
-    }
-    else if(functions=="Convolution"){
-        Convolve convolve(image);
-        cout << "Choose a specific operation! (Blur, Sharpen, Edge)" << endl;
-        cin >> str;
-        if(str=="Blur"){
-            convolve.blur(image);
-            image.image_name=image.image_name+"_blurred.pgm";
-            image.save();
-        }
-        else if(str=="Edge"){
-            convolve.edge(image);
-            image.image_name=image.image_name+"_edged.pgm";
-            image.save();
-        }
-        else if(str=="Sharpen"){
-            convolve.sharpen(image);
-            image.image_name=image.image_name+"_sharpened.pgm";
-            image.save();
-        }
-    }
-    cout << "\nDo you want to continue your work on other images? Press (y) or (n)!" << endl;
-    cin >> next;
-}
-
-/*catch(std::exception &e){
-    fprintf(stderr, e.what());
-    return 1;
-    }*/
-
-return 0;
+    Rotation rotated;
+    //rotated.load();
+    rotated.operation();
+    rotated.save();
+    return 0;
 }
